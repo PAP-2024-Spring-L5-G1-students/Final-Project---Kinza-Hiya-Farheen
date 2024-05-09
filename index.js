01a1b285e040140a87c5d5718b4927d145c7df0b
Kinza Anwar <kinzatanwar@gmail.com>
2024-05-09 16:51:35 -0700

    backend project completed by kinza, hiya, and farheen

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
+
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
-// const testing = "TopMascara";
 
 const PORT = 5500;
-const db = new sqlite3.Database('./brands.db');
+const db = new sqlite3.Database('./brandslinks.db');
 
 const app = express();
 
 app.use(cors()); 
 app.use(express.static("public"));
 
-app.get('/mascara', (req, res) => {
-    db.serialize(() => {
-        db.all(`SELECT ${testing} FROM MakeupBrands`, (err, rows) => {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send('Internal Server Error');
-            } else {
-                console.log("Query successful");
-                res.json(rows); 
-            }
-        });
-    });
-});
-
 app.get('/brand:option', (req, res) => {
     const testin = req.params.option;
-    // const testin = "Lawless";
 
     console.log(testin);
     const query = `SELECT * FROM MakeupBrands WHERE "BrandName" = ` +  `'` + `${testin}` + `';`;
@@ -51,7 +36,7 @@ app.get('/brand:option', (req, res) => {
 app.get('/product:option', (req, res) => {
     const testin = req.params.option;
     console.log(testin);
-    const query = `SELECT ` + `${testin}` + ` FROM MakeupBrands;`;
+    const query = `SELECT ` + `${testin}, ${testin}Link` + ` FROM MakeupBrands;`;
     console.log(query);
     db.serialize(() => {
         db.all(query, (err, rows) => {
@@ -68,10 +53,41 @@ app.get('/product:option', (req, res) => {
     });
 });
 
-app.get('/here', (req, res) => {
-    res.write("here it worked");
-    res.end();
-});
+app.get('/allBrands', (req, res) => {
+    const query = `SELECT BrandName FROM MakeupBrands ;`;
+    db.serialize(() => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                res.status(500).send('Internal Server Error');
+            } else {
+                console.log("Query successful");
+                req.params.get
+                res.json(rows); 
+                console.log(rows)
+            }
+        });
+    });
+})
+
+app.get('/search/:product/:brand', (req, res) => {
+    const query = `SELECT ` + `${req.params.product}, ${req.params.product}Link` + ` FROM MakeupBrands WHERE BrandName = "` + `${req.params.brand}` + `";`;
+    console.log(query)
+    db.serialize(() => {
+        db.all(query, (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                res.status(500).send('Internal Server Error');
+            } else {
+                console.log("Query successful");
+                req.params.get
+                res.json(rows); 
+                console.log(rows)
+            }
+        });
+    });
+})
+
 
 const server = app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
--- public/frontend.js
+++ public/frontend.js
@@ -1,19 +1,70 @@
 const somid = document.getElementById("som");
 const formid = document.getElementById("brand");
 const formid2 = document.getElementById("product");
+const brandSetup = document.getElementById("brands")
 
+// async function testgo() {
+//     const response = await fetch("http://localhost:5500/everything");
+
+// }
+
+async function dropDownSetup() {
+    const response = await fetch("http://localhost:5500/allBrands");
+    const data = await response.json();
+
+    data.forEach(item => {
+        brandSetup.innerHTML += `<option value="${item.BrandName}">${item.BrandName}</option>`;
+    })
+}
+
+async function getBoth(product, brand) {
+    const response = await fetch(`http://localhost:5500/search/` + product + `/${brand}`);
+
+    const data = await response.json();
+
+    data.forEach(item => {
+        if (item.TopFoundation != undefined && item.TopFoundation != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopFoundationLink}" >${item.TopFoundation}</a></div>`;
+        } else if (item.TopConcealer != undefined && item.TopConcealer != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopConcealerLink}" class="response">${item.TopConcealer}</a></div>`;
+        } else if (item.TopBlush != undefined && item.TopBlush != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopBlushLink}">${item.TopBlush}</a></div>`;
+        } else if (item.TopContour != undefined && item.TopContour != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopContourLink}">${item.TopContour}</a></div>`;
+        } else if (item.TopBrowProduct != undefined && item.TopBrowProduct != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopBrowProduct}">${item.TopBrowProduct}</a></div>`;
+        } else if (item.TopMascara != undefined && item.TopMascara != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopMascara}">${item.TopMascara}</a></div>`;
+        } else {
+            somid.innerHTML += `<div class="response">Product does not exist, try something else.</div>`;
+            console.log("nothing")
+        }
+    });
+}
 
 async function getBrand(val) {
     const response = await fetch("http://localhost:5500/brand" + val);
     const data = await response.json();
 
     data.forEach(item => {
-        somid.innerHTML += `<li>${item.TopFoundation}</li>`;
-        somid.innerHTML += `<li>${item.TopConcealer}</li>`;
-        somid.innerHTML += `<li>${item.TopBlush}</li>`;
-        somid.innerHTML += `<li>${item.TopContour}</li>`;
-        somid.innerHTML += `<li>${item.TopBrowProduct}</li>`;
-        somid.innerHTML += `<li>${item.TopMascara}</li>`;
+        if (item.TopFoundation != undefined && item.TopFoundation != 'null') {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopFoundationLink}" >${item.TopFoundation}</a></div>`;
+        }
+        if (item.TopConcealer != undefined && item.TopConcealer != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopConcealerLink}" class="response">${item.TopConcealer}</a></div>`;
+        }
+        if (item.TopBlush != undefined && item.TopBlush != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopBlushLink}">${item.TopBlush}</a></div>`;
+        }
+        if (item.TopContour != undefined && item.TopContour != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopContourLink}">${item.TopContour}</a></div>`;
+        }
+        if (item.TopBrowProduct != undefined && item.TopBrowProduct != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopBrowProduct}">${item.TopBrowProduct}</a></div>`;
+        }
+        if (item.TopMascara != undefined && item.TopMascara != "null") {
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopMascara}">${item.TopMascara}</a></div>`;
+        }
     });
 }
 
@@ -24,67 +75,37 @@ async function getProduct(val) {
     console.log(val)
     data.forEach(item => {
         if (item.TopFoundation != undefined && item.TopFoundation != "null") {
-            somid.innerHTML += `<li>${item.TopFoundation}</li>`;
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopFoundationLink}" >${item.TopFoundation}</a></div>`;
         } else if (item.TopConcealer != undefined && item.TopConcealer != "null") {
-            somid.innerHTML += `<li>${item.TopConcealer}</li>`;
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopConcealerLink}" >${item.TopConcealer}</a></div>`;
         } else if (item.TopBlush != undefined && item.TopBlush != "null") {
-            somid.innerHTML += `<li>${item.TopBlush}</li>`;
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopBlushLink}">${item.TopBlush}</a></div>`;
         } else if (item.TopContour != undefined && item.TopContour != "null") {
-            somid.innerHTML += `<li>${item.TopContour}</li>`;
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopContourLink}">${item.TopContour}</a></div>`;
         } else if (item.TopBrowProduct != undefined && item.TopBrowProduct != "null") {
-            somid.innerHTML += `<li>${item.TopBrowProduct}</li>`;
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopBrowProduct}">${item.TopBrowProduct}</a></div>`;
         } else if (item.TopMascara != undefined && item.TopMascara != "null") {
-            somid.innerHTML += `<li>${item.TopMascara}</li>`;
+            somid.innerHTML += `<div class="response"><a target="_blank" class="response" href="${item.TopMascara}">${item.TopMascara}</a></div>`;
         } else {
             console.log("nothing")
         }
     });
 }
-
-async function getBrandAndProduct(val) {
-    const response = await fetch("/product" + val);
-
-    const data = await response.json();
-    console.log(val)
-    data.forEach(item => {
-        if (item.TopFoundation != undefined && item.TopFoundation != "null") {
-            somid.innerHTML += `<li>${item.TopFoundation}</li>`;
-        } else if (item.TopConcealer != undefined && item.TopConcealer != "null") {
-            somid.innerHTML += `<li>${item.TopConcealer}</li>`;
-        } else if (item.TopBlush != undefined && item.TopBlush != "null") {
-            somid.innerHTML += `<li>${item.TopBlush}</li>`;
-        } else if (item.TopContour != undefined && item.TopContour != "null") {
-            somid.innerHTML += `<li>${item.TopContour}</li>`;
-        } else if (item.TopBrowProduct != undefined && item.TopBrowProduct != "null") {
-            somid.innerHTML += `<li>${item.TopBrowProduct}</li>`;
-        } else if (item.TopMascara != undefined && item.TopMascara != "null") {
-            somid.innerHTML += `<li>${item.TopMascara}</li>`;
-        } else {
-            console.log("nothing")
-        }
-    });
-}
-
-formid.addEventListener("submit", (e) => {
-    e.preventDefault();
-    somid.innerHTML = " ";
-
-        getBrand(document.querySelector("#brands").value);
-    console.log("submited")
-})
-
-formid.addEventListener("submit", (e) => {
-    e.preventDefault();
-    somid.innerHTML = " ";
-
-        getBrand(document.querySelector("#brands").value);
-    console.log("submited")
-})
 
 formid2.addEventListener("submit", (e) => {
     e.preventDefault();
     somid.innerHTML = " ";
-
-        getProduct(document.querySelector("#products").value);
-    console.log("submited")
+    if (document.querySelector("#products").value === "Blank") {
+        getBrand(document.querySelector("#brands").value)
+    } else if (document.querySelector("#brands").value === "Blank") {
+        getProduct(document.querySelector("#products").value)
+    } else {
+        getBoth(document.querySelector("#products").value, document.querySelector("#brands").value);
+    }
 })
+
+function dropDownSetups() {
+    dropDownSetup();
+}
+
+dropDownSetups();
